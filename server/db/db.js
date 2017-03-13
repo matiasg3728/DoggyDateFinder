6435349a713546ab52132e68944725b25a31e82a
ADEsposito <a.desposito640@gmail.com>
2017-03-13 12:51:08 -0500

    add schemas, and db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,1 +1,16 @@
-db.js
+var mongoose = require('mongoose');
+var connectionString = 'mongodb://localhost/dogdate'
+
+mongoose.connect(connectionString);
+
+mongoose.connection.on('connected', function(){
+	console.log("connected to "+ connectionString);
+})
+
+mongoose.connection.on('error', function(error){
+	console.log("MongoDB error "+ error);
+})
+
+mongoose.connection.on('disconnected', function(){
+	console.log("Mongoose disconnected from " + connectionString);
+})
