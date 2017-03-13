2c1c274eeec21d7278d2463f0e00f6dc102fb135
Matias Garcia <matiasgarcia@Matiass-MacBook-Air.local>
2017-03-13 13:12:21 -0500

    More structure finished, commence with the werk

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+
+require('./db/db');
+
+var DogController = require('./controllers/DogController');
+var UserController = require('./controllers/UserController');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use('/dogs', DogController);
+app.use('/user', UserController);
+
+server.listen(3000, function(){
+	console.log("Listening on port 3000");
+});
