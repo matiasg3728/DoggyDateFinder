57c330e5fa51bb5fd1d5682522ebbb2879fcc987
ADEsposito <a.desposito640@gmail.com>
2017-03-13 14:32:09 -0500

    update app.js in server dir to have bcrypt functionality

--- server/app.js
+++ server/app.js
@@ -4,20 +4,34 @@ var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
+var hbs = require('hbs');
+var session = require('express-session');
 
 require('./db/db');
-
-var DogController = require('./controllers/DogController');
-var UserController = require('./controllers/UserController');
-
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','hbs');
 
+app.use(session({
+	secret: " this is the secret salt",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {secure: false}
+}));
+
+var DogController = require('./controllers/DogController');
+var UserController = require('./controllers/UserController');
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+var DogController = require('./controllers/DogController');
+var UserController = require('./controllers/UserController');
 
 app.use('/dogs', DogController);
 app.use('/user', UserController);
